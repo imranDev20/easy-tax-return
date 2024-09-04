994c0039447775acf0a15fa28e1d27fa7e6a4245
Rahat393 <rahataminulislam37@gmail.com>
2024-09-04 11:14:16 +0600

    some fixing

--- app/(site)/_components/hero.tsx
+++ app/(site)/_components/hero.tsx
@@ -17,18 +17,20 @@ export default function Hero() {
               preparation, planning, and filing needs to thrive in today&apos;s
               competitive market.
             </p>
-            <Button className="mt-8 bg-[#0074D9] rounded-none">Discover</Button>
+            <Button className="mt-8 bg-[#0074D9] text-white rounded-none">
+              Discover
+            </Button>
           </div>
         </div>
         <div className="relative mt-8">
           <Image className=" rounded-t-full" src={heroImage} alt="hero-image" />
-          <div className="absolute top-4 left-4 bg-[#EBBDBD] text-[#513B3B] px-2 py-1 rounded-md">
+          <div className="absolute top-4 left-4 bg-[#EBBDBD] rounded-lg text-[#513B3B] px-2 py-1 ">
             Tax Advice
           </div>
-          <div className="absolute top-32 -right-8 bg-[#A8F1E5] text-teal-600 px-2 py-1 rounded-md">
+          <div className="absolute top-32 -right-8 bg-[#A8F1E5] rounded-lg text-teal-600 px-2 py-1  ">
             Legal Advice
           </div>
-          <div className="absolute bottom-4 -left-10 bg-[#F1EFA8] text-[#535237] px-2 py-1 rounded-md">
+          <div className="absolute bottom-4 -left-10 bg-[#F1EFA8] rounded-lg text-[#535237] px-2 py-1  ">
             Financial Advice
           </div>
         </div>
